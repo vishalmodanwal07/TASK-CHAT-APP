94116396c70008c9d7ec414146d5af8224129583
vishal modanwal <vk0764394@gmail.com>
2024-11-24 01:02:07 +0530

    searching feature

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -58,11 +58,27 @@ const authUser = asyncHandler(async (req, res) => {
 
 });
 
+// /api/user?search=piuysh
+//regex help us match strings
+const allUsers = asyncHandler(async(req, res)=>{
+  const keyword = req.query.search
+  ? {
+      $or: [
+        { name: { $regex: req.query.search, $options: "i" } },
+        { email: { $regex: req.query.search, $options: "i" } },
+      ],
+    }
+  : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+   
+
+})
 
 
 
 
 
 
-
-module.exports = { registerUser, authUser};
+module.exports = { registerUser, authUser , allUsers};
